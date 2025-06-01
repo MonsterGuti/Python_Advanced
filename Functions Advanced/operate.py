f8485e545c532876dd1db4e5a6af623ded4ccd54
MonsterGuti <gogulanovmartin@gmail.com>
2025-06-01 13:07:10 +0300

    Add operate() with +, -, *, / support via reducer

---
+++ Functions Advanced/operate.py
@@ -0,0 +1,60 @@
+def add(*args):
+    return reduce(lambda x, y: x + y, args)
+
+
+def subtract(*args):
+    return reduce(lambda x, y: x - y, args)
+
+
+def multiply(*args):
+    return reduce(lambda x, y: x * y, args)
+
+
+def divide(*args):
+    return reduce(lambda x, y: x / y, args)
+
+
+mapper = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+
+
+def operate(operator, *args):
+    return mapper[operator](*args)
+
+
+from functools import reduce
+
+
+def add(*args):
+    return reduce(lambda x, y: x + y, args)
+
+
+def subtract(*args):
+    return reduce(lambda x, y: x - y, args)
+
+
+def multiply(*args):
+    return reduce(lambda x, y: x * y, args)
+
+
+def divide(*args):
+    return reduce(lambda x, y: x / y, args)
+
+
+mapper = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+
+
+def operate(operator, *args):
+    return mapper[operator](*args)
+
+
+print(operate("+", 1, 2, 3))
