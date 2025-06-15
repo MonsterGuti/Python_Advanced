32fc7dd1852e393be94c11cbfc1930c6b9cddb97
MonsterGuti <gogulanovmartin@gmail.com>
2025-06-16 02:15:38 +0300

    Add Fibonacci sequence generator and number locator functionality

--- Modules/fibonacci/fibonacci_sequence.py
+++ Modules/fibonacci/fibonacci_sequence.py
@@ -1,5 +1,7 @@
 from Modules.fibonacci.fibonacci_module import create_sequence, locate
 
+fibonacci_sequence = []
+
 while True:
     command = input(">>> ")
     if command == "Stop":
@@ -12,14 +14,15 @@ while True:
         except ValueError:
             print("Invalid number.")
     elif command.startswith("Locate"):
+        if not fibonacci_sequence:
+            print("Please create the sequence first.")
+            continue
         try:
             number = int(command.split()[-1])
             num_position = locate(number, fibonacci_sequence)
-            print(num_position)
-        except ValueError:
-            print("Invalid number.")
-
             if num_position != -1:
-                print(f'The number is at position {num_position}.")
+                print(f"The number is at position {num_position}.")
             else:
                 print("The number is not in the sequence.")
+        except ValueError:
+            print("Invalid number.")
