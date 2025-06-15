29c153464e40d087bf185cbe28d70246a89361c7
MonsterGuti <gogulanovmartin@gmail.com>
2025-06-15 22:05:21 +0300

    importing math

--- Modules/Calculate_Logarithm.py
+++ Modules/Calculate_Logarithm.py
@@ -4,6 +4,6 @@ number = int(input())
 
 base = input()
 if base == "natural":
-    print(f'{math.log(number):.2f}')
+    print(f'{math.log(number):.3f}')
 else:
-    print(f'{math.log(number, int(base)):.2f}')
+    print(f'{math.log(number, int(base)):.3f}')
