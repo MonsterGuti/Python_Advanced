b98c7a1072900d3a1495f84e9de26e2b0313074a
MonsterGuti <gogulanovmartin@gmail.com>
2025-10-24 13:14:20 +0300

    telebit algorithm problem - sheep and saw

---
+++ telebit_pro/algorithm_problem.py
@@ -0,0 +1,21 @@
+N, K = map(int, input().split())
+sheep_list = list(map(int, input().split()))
+sheep_list.sort(reverse=True)
+courses_min = sum(sheep_list) / K
+min_values_courses = []
+
+sheep_index = 0
+for course in range(K):
+    current_course = 0
+    while sheep_index < len(sheep_list):
+        if current_course + sheep_list[sheep_index] <= courses_min:
+            current_course += sheep_list[sheep_index]
+            sheep_list.pop(sheep_index)
+        else:
+            sheep_index += 1
+    min_values_courses.append(current_course)
+
+for sheep in sheep_list:
+    min_values_courses[min_values_courses.index(min(min_values_courses))] += sheep
+
+print(max(min_values_courses))
